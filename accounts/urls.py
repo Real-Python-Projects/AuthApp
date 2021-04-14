6c0c8a1bc00eaf05e953a03bd46800d7e60ef6b5
Gateway47 <itsregalo047@gmail.com>
2021-04-14 23:41:46 +0300

    worked on auth urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import LogInView, LogOutView, RegisterView
+from .views import (LogInView, LogOutView, 
+                    RegisterView, ResetPasswordView, RequestResetEmail)
 
 app_name='accounts'
 
 urlpatterns = [
     path('login/', LogInView, name="login"),
     path('register/', RegisterView, name="register"),
+    path("logout/", LogOutView, name="logout"),
+    
+    
 ]
